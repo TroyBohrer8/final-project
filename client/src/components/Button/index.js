a107f380346f8c4bbebec57e5a78e86fa79d3b59
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-29 15:50:47 -0400

    added styling to the button

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -4,3 +4,29 @@ import {Link} from 'react-router-dom';
 
 const STYLES = ['btn--primary', 'btn--large'];
 
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => {
+    const checkButtonStyle =  STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+    return (
+        <Link to='/Signup' className='btn-mobile'>
+            <button 
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    );
+};
