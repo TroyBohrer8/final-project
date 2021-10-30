5c1e3ed25129e914c1989dd159c0f9cd13803abd
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-29 20:49:36 -0400

    added a booking model, and addedd images

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,9 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import Logo from './components/Logo';
+
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
